c54ead0cd8350131170e0cc01855a428b55b94ec
GwG <32935379+GwGibson@users.noreply.github.com>
2023-06-14 21:30:08 +0000

    Create minotaur tests

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -8,6 +8,8 @@ namespace MinotaurLabyrinthTest
         [TestMethod]
         public void PitRoomTest()
         {
+            // Seed the RandomNumberGenerator so the sequence of random numbers
+            // is deterministic
             RandomNumberGenerator.SetSeed(1);
 
             Pit pitRoom = new Pit();
@@ -29,11 +31,30 @@ namespace MinotaurLabyrinthTest
         }
     }
 
-    public class MonsterTests {
+    [TestClass]
+    public class MonsterTests
+    {
         [TestMethod]
-        public void MinotaurTest() {
+        public void MinotaurTest()
+        {
             Hero hero = new Hero();
             Minotaur minotaur = new Minotaur();
+            Map map = new Map(4, 4);
+            hero.HasSword = true;
+            Assert.AreEqual(hero.HasSword, true);
+
+            minotaur.Activate(hero, map);
+            // Charge moves the hero 1 room east and 2 rooms north
+            // -1 is off the map so hero position should be (0, 2)
+            Assert.AreEqual(hero.Location, new Location(0, 2));
+            Assert.AreEqual(hero.HasSword, false);
+
+            minotaur.Activate(hero, map);
+            Assert.AreEqual(hero.Location, new Location(0, 3));
+
+            hero.Location = new Location(3, 1);
+            minotaur.Activate(hero, map);
+            Assert.AreEqual(hero.Location, new Location(2, 3));
         }
     }
 }
